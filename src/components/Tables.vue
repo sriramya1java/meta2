2946b12b17cd4d14ee11c45585c6050dcf2d6b97
Sasi <sasi@p01r1w-3k1vvd2.ent.ds.gsa.gov>
2018-06-19 12:17:07 -0400

    adding tables

--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    <h1>displays the list of tables</h1>
-    <router-view></router-view>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+
+        <div class="row">
+          <div class="col-md-12">
+            <button type="button" class="btn btn-success float-left">Success</button>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <table class="table table-striped table-dark">
+              <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+        <button type="button" class="btn btn-outline-warning float-right">Success</button>
+      </div>
+    </div>
   </div>
 </template>
