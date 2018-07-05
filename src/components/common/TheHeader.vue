2d28806c015736cfc646928881f5c336c2e337d8
sriramya <sriramya.java@gmail.com>
2018-07-05 13:34:08 -0400

    updated changes to remove no break space

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -4,10 +4,8 @@
       <img src="../../assets/meta2logo.png">
     </div>
     <div class="col-4">
-      Welcome: {{ user }}
-      <a href="#" @click="logout">Logout</a>
-    <div class="d-inline">Welcome: {{ user }}</div>
-    <div class="d-inline"><a href="#" @click="logout">Logout</a></div>
+    Welcome: {{ user }}
+    <a href="#" @click="logout">Logout</a>
     </div>
   </div>
 </template>
