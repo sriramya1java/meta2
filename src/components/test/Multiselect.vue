364bc1bd0e620796ccbe5ab31b2af6d29ddbc473
skesani <kesanisasikanth@gmail.com>
2018-08-15 20:17:04 -0600

    fixed Multiselect enjoy

--- src/components/test/Multiselect.vue
+++ src/components/test/Multiselect.vue
@@ -1,41 +1,96 @@
 <template>
-    <div class="row">
-        <div class="col-xs-5">
-            <select name="from" id="multiselect" class="form-control" size="8" multiple="multiple">
-                <option value="1">C++</option>
-                <option value="2">C#</option>
-                <option value="3">Haskell</option>
-                <option value="4">Java</option>
-                <option value="5">JavaScript</option>
-                <option value="6">Lisp</option>
-                <option value="7">Lua</option>
-                <option value="8">MATLAB</option>
-                <option value="9">NewLISP</option>
-                <option value="10">PHP</option>
-                <option value="11">Perl</option>
-                <option value="12">SQL</option>
-                <option value="13">Unix shell</option>
-            </select>
+    <div>
+        <div class="row align-items-center justify-content-center">
+            <div class="col-xs-3 alert alert-warning" v-if="showalert" style="margin: 10px;" role="alert">
+                Please select at lease one option
+            </div>
         </div>
-        <div class="col-xs-2">
-            <button type="button" id="multiselect_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
-            <button type="button" id="multiselect_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
-            <button type="button" id="multiselect_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
-            <button type="button" id="multiselect_leftAll" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
-        </div>
-        <div class="col-xs-5">
-            <select name="to" id="multiselect_to" class="form-control" size="8" multiple="multiple">
-            </select>
+        <div class="row">
+            <div class="col-xs-5">
+                <select class="form-control" id="leftOptions" v-model="leftSelected" multiple="multiple" style="min-height: 150px;background:#f8f8f8; color: #0d47a1">
+                    <option v-for="optionLeft in leftOptions" v-bind:value="optionLeft">{{ optionLeft.countryName }}</option>
+                </select>
+            </div>
+            <div class="col-xs-2">
+                <button type="button" id="multiselect_rightAll" class="btn btn-block" v-on:click="multiselect_rightAll()"><i class="glyphicon glyphicon-forward"></i></button>
+                <button type="button" id="multiselect_rightSelected" class="btn btn-block"  v-on:click="multiselect_rightSelected()"><i class="glyphicon glyphicon-chevron-right"></i></button>
+                <button type="button" id="multiselect_leftSelected" class="btn btn-block" v-on:click="multiselect_leftSelected()"><i class="glyphicon glyphicon-chevron-left"></i></button>
+                <button type="button" id="multiselect_leftAll" class="btn btn-block"  v-on:click="multiselect_leftAll()"><i class="glyphicon glyphicon-backward"></i></button>
+            </div>
+            <div class="col-xs-5">
+                <select class="form-control" id="dataset_id" v-model="rightSelected"  multiple="multiple" style="min-height: 150px;background:#f8f8f8; color: #ef6c00">
+                    <option v-for="optionRight in rightOptions" v-bind:value="optionRight">{{ optionRight.countryName }}</option>
+                </select>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    import jQuery from 'jquery'
-    jQuery(document).ready(function ($) {
-      $('#multiselect').multiselect()
-    })
     export default {
-      name: 'multiselect'
+      name: 'multiselect',
+      data () {
+        return {
+          leftOptions: [
+                    {countryCode: 'AU', countryName: 'Australia'},
+                    {countryCode: 'CA', countryName: 'Canada'},
+                    {countryCode: 'CN', countryName: 'China'},
+                    {countryCode: 'DE', countryName: 'Germany'},
+                    {countryCode: 'JP', countryName: 'Japan'},
+                    {countryCode: 'MX', countryName: 'Mexico'},
+                    {countryCode: 'CH', countryName: 'Switzerland'},
+                    {countryCode: 'US', countryName: 'United States'}
+          ],
+          rightOptions: [],
+          leftSelected: [],
+          rightSelected: [],
+          showalert: false
+        }
+      },
+      methods: {
+        multiselect_rightAll: function () {
+          this.leftOptions.forEach(x => {
+            const rightIndex = this.rightOptions.findIndex(z => z === x)
+            if (!(rightIndex > -1)) {
+              this.rightOptions.push(x)
+            }
+          })
+          this.leftOptions = []
+          this.showalert = false
+        },
+        multiselect_leftAll: function () {
+          this.rightOptions.forEach(x => {
+            const rightIndex = this.leftOptions.findIndex(z => z === x)
+            if (!(rightIndex > -1)) {
+              this.leftOptions.push(x)
+            }
+          })
+          this.rightOptions = []
+          this.showalert = false
+        },
+        multiselect_rightSelected: function () {
+          this.showalert = this.leftSelected.length === 0
+          this.leftSelected.forEach(x => {
+            const leftIndex = this.leftOptions.findIndex(y => y === x)
+            this.leftOptions.splice(leftIndex, 1)
+            const rightIndex = this.rightOptions.findIndex(z => z === x)
+            if (!(rightIndex > -1)) {
+              this.rightOptions.push(x)
+            }
+          })
+        },
+        multiselect_leftSelected: function () {
+          this.showalert = this.rightSelected.length === 0
+          this.rightSelected.forEach(x => {
+            const rightIndex = this.rightOptions.findIndex(y => y === x)
+            this.rightOptions.splice(rightIndex, 1)
+            const leftIndex = this.leftOptions.findIndex(z => z === x)
+            if (!(leftIndex > -1)) {
+              this.leftOptions.push(x)
+            }
+          })
+        }
+
+      }
     }
 </script>
